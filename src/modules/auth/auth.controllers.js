aa60160775c1350c6fd763e5023f9a3143bb02ff
munir79 <jakirhossainmunir79@gmail.com>
2025-05-31 18:09:02 +0600

    added

--- src/modules/auth/auth.controllers.js
+++ src/modules/auth/auth.controllers.js
@@ -1,20 +1,33 @@
-import { AuthService } from "./auth.service.js";
+import { AuthService } from './auth.service.js';
 
+const CreateUSerControllers = async (req, res, next) => {
+  try {
+    const userData = req.body;
+    const cretaeUser = await AuthService.registarUser(userData);
+    res.status(200).json({
+      sucess: true,
+      message: ' User Created SucesFully',
+      data: cretaeUser,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
-const CreateUSerControllers=async(req, res, next)=>{
-    try{
-        const userData=req.body;
-        const cretaeUser=await AuthService.registarUser(userData);
-        res.status(200).json({
-            sucess:true,
-            message:" User Created SucesFully",
-            data:cretaeUser
-        })
+// sign in user
 
-    }
-    catch(err){
-        next(err)
-    }
-}
+const signIn = async (req, res, next) => {
+  try {
+    const user = req.body;
+    const result = await AuthService.loginUser(user);
+    res.status(200).json({
+      sucess: true,
+      message: 'log in successFully',
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const AuthControllers={CreateUSerControllers}
+export const AuthControllers = { CreateUSerControllers };
--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,34 +1,71 @@
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
 import prisma from '../../prisma/prismaClient.js';
+import jwt from 'jsonwebtoken';
 
-const registarUser=async(userData)=>{
-   try{
-     const hashPassword=await bcrypt.hash(userData.password,10);
-    const isExist=await prisma.user.findUnique({
-        where:{email:userData.email},
+const JWT_SECRECTKEY = process.env.JWT_SECRET;
+const registarUser = async (userData) => {
+  try {
+    const hashPassword = await bcrypt.hash(userData.password, 10);
+    const isExist = await prisma.user.findUnique({
+      where: { email: userData.email },
     });
 
-    if(isExist){
-        throw new Error(400, " user Already Exissts");
-    };
+    if (isExist) {
+      throw new Error(400, 'user Already Exissts');
+    }
 
-    const CretaeUser=await prisma.user.create({
-        data :{
-            name:userData.name,
-            email:userData.email,
-            password:hashPassword
-        }
-    })
+    const CretaeUser = await prisma.user.create({
+      data: {
+        name: userData.name,
+        email: userData.email,
+        password: hashPassword,
+      },
+    });
 
-    return CretaeUser
+    return CretaeUser;
+  } catch (err) {
+    throw new Error('failed to create user ');
+  }
+};
 
-   }
-   catch(err){
-    throw new Error("failed to create user ");
+// log in user
 
-   }
+const loginUser = async (userData) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      throw new Error(404, ' user not found');
+    }
+    const isMatch = await bcrypt.compare(userData.password, user.password);
+    if (!isMatch) {
+      throw new Error(' incorrect password');
+    }
 
-}
+    // create jwt token
 
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        email: user.email,
+      },
+      JWT_SECRECTKEY,
+      {
+        expiresIn: '7d',
+      }
+    );
 
-export const AuthService={registarUser};
+    //remove password before returning 
+    const {password:_,...userWithoutPassword }=user;
+
+    return{
+        token,
+        user:userWithoutPassword
+    }
+  } catch (err) {
+    throw new Error('No User ');
+  }
+};
+
+export const AuthService = { registarUser , loginUser};
